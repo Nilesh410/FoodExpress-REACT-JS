67505f84bca8674e8eb81e90d3cb5653b3557ec1
Nilesh410 <shirudenileshs@gmail.com>
2024-01-30 00:38:43 +0530

    Adding Food Page

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
+import FoodPage from './pages/FoodPage/FoodPage';
 import HomePage from './pages/Home/HomePage';
 export default function AppRoutes() {
   return (
@@ -7,6 +8,7 @@ export default function AppRoutes() {
         <Route path="/" element={<HomePage/>}/>
         <Route path="/search/:searchTerm" element={<HomePage />} />
         <Route path="/tag/:tag" element={<HomePage />} />
+        <Route path='/food/:id' element={<FoodPage />} />
     </Routes>
   );
 }
---
+++ frontend/src/components/NotFound/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import classes from './notFound.module.css';
+export default function NotFound() {
+  return (
+    <div>
+       NotFound
+    </div>
+  )
+}
