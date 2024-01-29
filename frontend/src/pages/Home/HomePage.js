70a65e448d4b14dcd2d3b956408c65311c64cc73
Nilesh410 <shirudenileshs@gmail.com>
2024-01-29 23:09:21 +0530

    Adding tag bar and showing the foods as per tag

--- frontend/src/pages/Home/HomePage.js
+++ frontend/src/pages/Home/HomePage.js
@@ -1,17 +1,19 @@
 import React, { useEffect } from 'react'
 import { useReducer } from 'react';
 import Thumbnails from '../../components/Thumnails/Thumbnails';
-import { getAll } from '../../services/foodService';
+import { getAll, getAllByTag, getAllTags } from '../../services/foodService';
 import { useParams } from 'react-router-dom';
 import { search } from '../../services/foodService';
 import Search from '../../components/Search/Search';
+import Tags from '../../components/Tags/Tags';
 //reducer
-const initialState = { foods: []};
+const initialState = { foods: [],tags:[]};
 const reducer = (state, action) => {
     switch (action.type) {
       case 'FOODS_LOADED':
         return { ...state, foods: action.payload };
-      
+        case 'TAGS_LOADED':
+          return { ...state, tags: action.payload };
       default:
         return state;
     }
@@ -19,16 +21,20 @@ const reducer = (state, action) => {
 export default function HomePage() {
   
     const [state, dispatch] = useReducer(reducer, initialState);  
-    const {foods}=state;
-    const {searchTerm}=useParams();
+    const {foods,tags}=state;
+    const {searchTerm,tag}=useParams();
 
     useEffect(()=>{
-      const loadFoods=searchTerm? search(searchTerm):getAll();
+      getAllTags().then (tags=>dispatch({type:'TAGS_LOADED',payload:tags}));
+      const loadFoods=
+      tag?getAllByTag(tag):
+      searchTerm? search(searchTerm):getAll();
       loadFoods.then(foods=>dispatch({type:'FOODS_LOADED',payload:foods}));
-    },[searchTerm]);
+    },[searchTerm,tag]);
     return (
       <>
         <Search />
+        <Tags tags={tags}/>
         <Thumbnails foods={foods}/>
       </>
     );
