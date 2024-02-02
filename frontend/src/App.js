07d108d595485e1ead86607678d495d93150b46b
Nilesh410 <shirudenileshs@gmail.com>
2024-02-02 20:24:10 +0530

    16. Loading!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import AppRoutes from "./AppRoutes";
 import Header from "./components/Header/Header";
-
+import Loading from "./components/Loading/Loading.js";
+import  { useLoading} from './hooks/useLoading';
+import { setLoadingInterceptor} from './interceptors/loadingInterceptor';
+import { useEffect } from 'react';
 function App() {
+  const { showLoading, hideLoading } = useLoading();
+
+  useEffect(() => {
+    setLoadingInterceptor({ showLoading, hideLoading });
+  }, []);
   return (
     <>
+    <Loading />
     <Header />
     <AppRoutes/>
     </>
